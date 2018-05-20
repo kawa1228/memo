fe542301462fad093c8a461261eeb4dbaf3c60c6
kawasumi <ayakawasumi@Ayas-MacBook-Pro.local>
2018-05-20 15:29:44 +0900

    HomeとEditorの条件分岐とGoogleログイン

---
+++ memomemo/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div id ="home">
+    <h1>{{ msg }}</h1>　<!--@clickはHTMLのonClickみたいなもの-->
+    <button @click="googleLogin">Google アカウントでログイン</button>
+</div>
+</template>
+
+<script>
+export default {
+    name:'home',
+    data(){
+        return{
+            msg:'Welcome to Memo'
+        }
+    },
+    //googleLoginという関数の内容を記載
+    methods:{
+        googleLogin:function(){
+            firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+        }
+    }
+}
+</script>
