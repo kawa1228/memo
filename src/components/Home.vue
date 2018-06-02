07143d5299917926254956f7971df9fddf5a1a8c
kawa1228 <sxa.1105bo@gmail.com>
2018-06-02 18:22:08 +0900

    cssの作成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
 <div id ="home">
+    <title>{{ titile }}</title>
     <h1>{{ msg }}</h1>　<!--@clickはHTMLのonClickみたいなもの-->
+    <h2>オンラインで簡単メモ作成</h2>
     <button @click="googleLogin">Google アカウントでログイン</button>
+    <ul>
+        <li>
+            <a href="#">利用規約</a>
+        </li>
+        <li>
+            <a href="#">プライバシーポリシー</a>
+        </li>
+    </ul>
 </div>
 </template>
 
@@ -10,7 +20,8 @@ export default {
     name:'home',
     data(){
         return{
-            msg:'Welcome to Memo'
+            msg:'Welcome to Memo',
+            title:'めも'
         }
     },
     //googleLoginという関数の内容を記載
